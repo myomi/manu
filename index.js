3dcdbeda2ad3ba8c7d0eb6c8985c1c288382245f
myomi <myomit@gmail.com>
2018-02-24 10:02:56 +0900

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Manu = /** @class */ (function () {
+    function Manu() {
+        this.version = '0.0.1';
+    }
+    Manu.prototype.start = function () {
+        console.log('hello');
+    };
+    return Manu;
+}());
+exports.default = Manu;
---
+++ index.ts
@@ -0,0 +1,7 @@
+export default class Manu {
+    version: string = '0.0.1';
+
+    start() {
+        console.log('hello');
+    }
+}
